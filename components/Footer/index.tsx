2577858a9d34cb5d41ebfcff5c258a50115760e1
Fedir Melnyk <pluserwork24@gmail.com>
2023-08-02 23:58:20 +0300

    upd

---
+++ components/Footer/index.tsx
@@ -0,0 +1,10 @@
+'use client'
+import { Footer } from "antd/es/layout/layout"
+
+export default function FooterComponent() {
+    return (
+        <Footer>
+            footer pidoras
+        </Footer>
+    );
+}
