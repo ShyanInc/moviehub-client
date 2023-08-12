c011177da7b820d44dea6c924203579ab55ab723
trusthemind <pluserwork24@gmail.com>
2023-08-12 23:55:34 +0300

    header fix

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -2,16 +2,22 @@
 import { Header } from "antd/es/layout/layout";
 import s from "./style.module.sass"
 import Link from "next/link";
+import { Input } from "antd"
 
 export default function HeaderComponent() {
     return (
         <Header className={s.header}>
             <h1 className={'text_logo'}>MovieHub</h1>
-            <div>
+            <div className={s.section}>
                 <Link href={""}>Series</Link>
                 <Link href={""}>Films</Link>
             </div>
-            <div></div>
+            <div>
+                <Input
+                    className={s.search}
+                    placeholder="Search"
+                    allowClear />
+            </div>
         </Header>
     );
 }
