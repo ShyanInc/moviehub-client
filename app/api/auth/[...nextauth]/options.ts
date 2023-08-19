01baff2aefd3c12a148131f15a11a90ebc77fcc5
Shyan <shyan.inc@gmail.com>
2023-08-19 18:44:03 +0300

    Refactor options.ts

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,48 +1,43 @@
-import type { NextAuthOptions, User } from "next-auth"
-import GitHubProvider from "next-auth/providers/github"
-import CredentialsProvider from "next-auth/providers/credentials"
-import { json } from "stream/consumers"
+import type { NextAuthOptions } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
 
 export const options: NextAuthOptions = {
   providers: [
-    // GitHubProvider({
-    //   clientId: process.env.GITHUB_ID as string,
-    //   clientSecret: process.env.GITHUB_SECRET as string
-    // }),
     CredentialsProvider({
-      name: "credentials",
+      name: 'credentials',
       credentials: {
         email: {
-          label: "Email",
-          type: "email",
+          label: 'Email',
+          type: 'email',
           reqiered: true,
         },
         password: {
-          label: "Password",
-          type: "password",
+          label: 'Password',
+          type: 'password',
           reqiered: true,
         },
       },
       async authorize(credentials) {
-        const res = await fetch("https://moviehub-api-8l85.onrender.com/auth/login",
+        const res = await fetch(
+          'https://moviehub-api-8l85.onrender.com/auth/login',
           {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
               email: credentials?.email,
-              password: credentials?.password
-            })
-          })
+              password: credentials?.password,
+            }),
+          }
+        );
         const user = await res.json();
-        console.log(user)
-        if (user)
-          return user
-        else
-          throw new Error("Error");
-          
-      }
-    })
+        console.log(user);
+        if (user) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
   ],
-  pages:
-    { signIn: '/auth/signin' }
-}
+  pages: { signIn: '/auth/signin', signOut: '/auth/signout' },
+};
