7e7acb5a35ab8756abe291a97e9e2b4e448dec66
Shyan <shyan.inc@gmail.com>
2023-08-24 18:39:56 +0300

    Modify Header styles

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,36 +1,38 @@
 'use client';
-import { Header } from 'antd/es/layout/layout'
-import s from './style.module.sass'
-import Link from 'next/link'
-import { Input } from 'antd'
-import { SearchOutlined } from '@ant-design/icons/lib/icons'
-import { useState } from 'react'
+import { Header } from 'antd/es/layout/layout';
+import s from './style.module.sass';
+import Link from 'next/link';
+import { Input } from 'antd';
+import { SearchOutlined } from '@ant-design/icons/lib/icons';
+import { useState } from 'react';
 import UserAvatar from '../UserAvatar';
 
 export default function HeaderComponent() {
-    const [search, setSearch] = useState("")
+  const [search, setSearch] = useState('');
 
-    return (
-        <Header className={s.header + ' container'}>
-            <Link href={"/"}><h1 className={'text_logo'}>MovieHub</h1></Link>
-            <div className={s.section}>
-                <Link href={''}>Series</Link>
-                <Link href={''}>Films</Link>
-            </div>
-            
-            <div>
-                <div className={s.user_search}>
-                <div>
-                        <Input
-                            className={s.search}
-                            placeholder="Search"
-                            onInput={(e) => setSearch(e.currentTarget.value)}  
-                            />
-                            {search.length < 1 && <SearchOutlined className={s.search_icon}/>}
-                    </div>
-                    <UserAvatar/>
-                </div>
-            </div>
-        </Header>
-    );
+  return (
+    <Header className={s.header + ' container'}>
+      <Link href={'/'}>
+        <h1 className={'text_logo'}>MovieHub</h1>
+      </Link>
+      <div className={s.navigation}>
+        <Link href={''}>Movies</Link>
+        <Link href={''}>Series</Link>
+      </div>
+
+      <div>
+        <div className={s.user_search}>
+          <div className={s.search_block}>
+            <Input
+              className={s.search}
+              placeholder='Search'
+              onInput={(e) => setSearch(e.currentTarget.value)}
+            />
+            {search.length < 1 && <SearchOutlined className={s.search_icon} />}
+          </div>
+          <UserAvatar />
+        </div>
+      </div>
+    </Header>
+  );
 }
