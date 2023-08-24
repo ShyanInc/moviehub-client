980dd32b1dee76bbf52d549c32e7b43f92a9b7ba
Shyan <shyan.inc@gmail.com>
2023-08-24 22:10:45 +0300

    Fix var naming & Refactor Movies Slice

--- redux/slices/moviesSlice.ts
+++ redux/slices/moviesSlice.ts
@@ -1,6 +1,5 @@
-import { moviesApi } from '@/api/api';
 import { Movie } from '@/types';
-import { PayloadAction, createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 
 export interface moviesState {
   movies: Movie[];
@@ -10,11 +9,6 @@ const initialState: moviesState = {
   movies: [],
 };
 
-export const getMovies = createAsyncThunk('movies/getMovies', async () => {
-  const movies = await moviesApi.getMovies();
-  return movies;
-});
-
 export const moviesSlice = createSlice({
   name: 'movies',
   initialState,
@@ -23,14 +17,7 @@ export const moviesSlice = createSlice({
       state.movies = action.payload;
     },
   },
-  extraReducers: (builder) => {
-    builder.addCase(getMovies.fulfilled, (state, action) => {
-      state.movies = action.payload;
-    });
-  },
 });
 
 export const { setMovies } = moviesSlice.actions;
 export default moviesSlice.reducer;
-// export const { setMovies } = moviesSlice.actions;
-// export default moviesSlice.reducer;
