8f5fe1ab4d427e47307525e8da1df66e3ac86511
Shyan <shyan.inc@gmail.com>
2023-08-26 02:42:58 +0300

    Add signout redirect to the home page

--- components/UserAvatar/index.tsx
+++ components/UserAvatar/index.tsx
@@ -1,19 +1,31 @@
-'use client'
-import s from "../Header/style.module.sass"
-import { useEffect } from "react";
-import { UserOutlined } from "@ant-design/icons/lib/icons";
-import Avatar from "antd/es/avatar/avatar";
-import { signOut, useSession } from "next-auth/react";
-import Link from "next/link";
+'use client';
+import s from '../Header/style.module.sass';
+import { useEffect } from 'react';
+import { UserOutlined } from '@ant-design/icons/lib/icons';
+import Avatar from 'antd/es/avatar/avatar';
+import { signOut, useSession } from 'next-auth/react';
+import Link from 'next/link';
 
 export default function UserAvatar() {
   const session = useSession();
 
   return (
     <div className={s.Auth}>
-      {!session.data && <><Avatar icon={<UserOutlined />} /><p>Guest</p><Link href="/api/auth/signin">Sign In</Link></>}
-      {session.data && <Link href="#" onClick={() => signOut({ callbackUrl: "/" })}>Sign Out</Link>}
+      {!session.data && (
+        <>
+          <Avatar icon={<UserOutlined />} />
+          <p>Guest</p>
+          <Link href='/api/auth/signin'>Sign In</Link>
+        </>
+      )}
+      {session.data && (
+        <Link
+          href='#'
+          onClick={() => signOut({ callbackUrl: '/', redirect: true })}
+        >
+          Sign Out
+        </Link>
+      )}
     </div>
   );
 }
-
