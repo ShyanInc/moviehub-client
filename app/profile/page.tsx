529caeed68191eb1ec10846e9eed9575a0a91a66
Fedir Melnyk <pluserwork24@gmail.com>
2023-09-20 13:41:08 +0300

    fix profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,26 +1,27 @@
 'use client';
 import { Card, Avatar } from 'antd';
 import s from './style.module.sass';
-import { getSession, signIn } from "next-auth/react"
+import { getSession, signIn } from 'next-auth/react';
 import { useState, useEffect } from 'react';
 import Preloader from '../loading';
 export default function Profile() {
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
 
-  const securePage = async () => {
-    const session = await getSession();
-    if (!session)
-      signIn();
-    else
-    setLoading(!loading)
-  }
+  // const securePage = async () => {
+  //   const session = await getSession();
+  //   if (!session)
+  //     signIn();
+  //   else
+  //   setLoading(!loading)
+  // }
 
-  useEffect(() => {
-    securePage()
-  }, [])
+  // useEffect(() => {
+  //   securePage()
+  // // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [])
 
-  if(loading)
-    return <Preloader/>;
+  // if(loading)
+  //   return <Preloader/>;
 
   return (
     <main>
@@ -31,15 +32,18 @@ export default function Profile() {
               className={s.avatar}
               src='https://avatars.githubusercontent.com/u/123456789?v=4'
             />
-            <h3>{"user#12"}</h3>
+            <h3>{'user#12'}</h3>
             <h1>{'UserName'}</h1>
           </div>
           <div className={s.bottomUserCard}>
-            <div className={s.watchedTracker}>
-              <p>1</p>
-              <p>1</p>
+            <div>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat commodi sequi nam animi culpa recusandae.</p>
             </div>
-            <p className={s.userRegistrated}>23.444.4545</p>
+            <div>
+              <p>Watched films:<br/><span>1</span></p>
+              <p>Watched series:<br/><span>1</span></p>
+            </div>
+            <p className={s.userRegistrated}>with us from: {"11.12.2022"}</p>
           </div>
         </Card>
         <Card className={s.lastWatchedCard}></Card>
