6fa2d900f74f3716ed71a5428853ecf39fce2ef0
Fedir Melnyk <pluserwork24@gmail.com>
2023-09-20 10:44:36 +0300

    add not found

---
+++ components/SVG/Not-Found.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import svgImage from "./not-found.svg"
+export const PageNotFound = () => {
+    return (
+        <Image src={svgImage.src}
+            width={500}
+            height={500}
+            alt="Not Found Icon"
+        ></Image>
+    );
+};
